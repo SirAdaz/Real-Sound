917d705589fc7e869c70be0b8c86bee5267dc13a
Adaz SirAdaz <mrxptdrgame@gmail.com>
2024-07-09 13:06:59 +0200

    page produits fait

--- js/pageProduits.js
+++ js/pageProduits.js
@@ -1,5 +1,10 @@
 let d = new Date();
 let fullDate;
+let user = localStorage.getItem("datauser");
+let objUser = JSON.parse(user);
+
+document.getElementById("firstName").innerText = objUser.firstname;
+
 function date() {
     let DD = d.getDate();
     let MM = d.getMonth() + 1;
@@ -24,9 +29,74 @@ function date() {
     }
     fullDate = DD + '/' + MM + '/' + YYYY + " " + hh + ":" + mm + ":" + ss;
     return fullDate;
-}
+};
 fullDate = date();
-let user = localStorage.getItem("datauser");
-let objUser = JSON.parse(user)
-document.getElementById("firstName").innerText = objUser.firstname;
 document.getElementById("date").innerText = fullDate;
+
+
+let btnBillie = document.getElementById("btn-billie");
+let btnTaylor = document.getElementById("btn-taylor");
+let btnWeeknd = document.getElementById("btn-weeknd");
+let objPanier = JSON.parse(localStorage.getItem("dataPanier")) || {};
+let billie;
+let taylor;
+let weeknd;
+
+function dataBillie() {
+    if (typeof objPanier.billie !== "number") {
+        billie = 0;
+    } else {
+        billie = objPanier.billie;
+    }
+    return billie;
+}
+
+function dataTaylor() {
+    if (typeof objPanier.taylor !== "number") {
+        taylor = 0;
+    } else {
+        taylor = objPanier.taylor;
+    }
+    return taylor;
+}
+
+function dataWeeknd() {
+    if (typeof objPanier.weeknd !== "number") {
+        weeknd = 0;
+    } else {
+        weeknd = objPanier.weeknd;
+    }
+    return weeknd;
+}
+
+billie = dataBillie();
+taylor = dataTaylor();
+weeknd = dataWeeknd();
+
+let panier = {
+    billie: billie,
+    taylor: taylor,
+    weeknd: weeknd
+};
+
+btnBillie.addEventListener("click", function(e) { 
+    e.preventDefault();
+    billie++;
+    panier.billie = billie;
+    localStorage.setItem("dataPanier", JSON.stringify(panier)); 
+});
+
+btnTaylor.addEventListener("click", function(e) { 
+    e.preventDefault();
+    taylor++;
+    panier.taylor = taylor;
+    localStorage.setItem("dataPanier", JSON.stringify(panier)); 
+});
+
+btnWeeknd.addEventListener("click", function(e) { 
+    e.preventDefault();
+    weeknd++;
+    panier.weeknd = weeknd;
+    localStorage.setItem("dataPanier", JSON.stringify(panier)); 
+});
+
